0691f77fc7b014df2dfd78f5a2095ad1f2928121
EraZz187 <becir.bahtijari@hotmail.com>
2025-05-17 18:46:56 +0200

    FirstUpload

---
+++ .pio/build/esp32doit-devkit-v1/libff3/MPU9250_WE/xyzFloat.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/libff3/MPU9250_WE/xyzFloat.cpp.o: \
+ .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.cpp \
+ .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.h \
+ .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.h
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//Wifi Connection
+const char* ssid = "TurkishAirlines_";
+const char* password = "123Polizei";
+const char* hostname = "eCompass";
+
+
+void setup(void) {
+  Serial.begin(115200); 
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_SPIFFS
+      type = "filesystem";
+    }
+
+    
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.setHostname(hostname);
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (!MDNS.begin(hostname)) // electronicCompass.local
+  {
+    Serial.println("Error setting up mDNS.");
+  }
+  MDNS.addService("http", "tcp", 80);
+}
+
+void loop(void) {
+  ArduinoOTA.handle();
+}
