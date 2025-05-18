5a4b456b7702a42e240428e353738a8ad558665c
EraZz187 <becir.bahtijari@hotmail.com>
2025-05-18 22:09:25 +0200

    Added GPSTinyPlus Library

--- .pio/build/esp32doit-devkit-v1/libff3/MPU9250_WE/xyzFloat.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/libff3/MPU9250_WE/xyzFloat.cpp.o: \
- .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.cpp \
- .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.h \
- .pio/libdeps/esp32doit-devkit-v1/MPU9250_WE/src/xyzFloat.h
